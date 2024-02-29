26422c17e8bd3dc7a958ecb2cc37fc3bf7f83305
Simen Haug <simehaug@stud.ntnu.no>
2024-02-29 11:51:41 +0100

    added untested slave communication package

---
+++ project/master/slavecomm/slavecomm.go
@@ -0,0 +1,68 @@
+package slavecomm
+
+import (
+	"encoding/json"
+	"fmt"
+	"master/community"
+	"net"
+)
+
+func Receiver(slaveConn *net.TCPConn, ch chan<- community.SlaveMessage) {
+	remoteIP := slaveConn.RemoteAddr().String()
+	buffer := make([]byte, 1024)
+
+	for {
+		// Read data from the client
+		n, err := slaveConn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		//attempt to unmarshal to different formats:
+		var data interface{}
+		var elevatorState community.ElevatorState
+		var buttonEvent community.ButtonEvent
+		var orderComplete community.OrderComplete
+
+		if err = json.Unmarshal(buffer[:n], &elevatorState); err == nil {
+			data = elevatorState
+		} else if err = json.Unmarshal(buffer[:n], &buttonEvent); err == nil {
+			data = buttonEvent
+		} else if err = json.Unmarshal(buffer[:n], &orderComplete); err == nil {
+			data = orderComplete
+		} else {
+			fmt.Println("json.Unmarshal error (no matching data types) : ", err)
+			return
+		}
+
+		if data != nil {
+			ch <- community.SlaveMessage{
+				SenderIP: remoteIP,
+				Payload:  data,
+			}
+		}
+
+	}
+
+}
+
+func Sender(slaveConn *net.TCPConn, ch <-chan json.Marshaler) {
+
+	for {
+		data := <-ch
+		jsonBytes, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println("json.Marshal error: ", err)
+			return
+		}
+
+		_, err = slaveConn.Write(jsonBytes)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+	}
+
+}
