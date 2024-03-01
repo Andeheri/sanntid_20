be825dd7d08a35278a754e16334dea53407d221b
Simen Haug <simehaug@stud.ntnu.no>
2024-03-01 18:34:31 +0100

    redid slavecomm

--- project/master/slavecomm/slavecomm.go
+++ project/master/slavecomm/slavecomm.go
@@ -7,8 +7,94 @@ import (
 	"project/master/community"
 )
 
-func Receiver(slaveConn *net.TCPConn, ch chan<- community.SlaveMessage) {
+func Listener(port int) {
+
+	localAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	listener, err := net.ListenTCP("tcp", localAddr)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	for {
+
+		slaveConn, err := listener.AcceptTCP()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		handleSlave(slaveConn, nil, nil)
+	}
+
+}
+
+func handleSlave(slaveConn *net.TCPConn, toSlaveCh <-chan json.Marshaler, toMasterCh chan<- interface{}) {
+	defer slaveConn.Close()
+
 	remoteIP := slaveConn.RemoteAddr().String()
+
+	tcpReadCh := make(chan []byte)
+	quitCh := make(chan struct{})
+
+	go tcpReader(slaveConn, tcpReadCh, quitCh)
+
+	for {
+		select {
+		case <-quitCh:
+			toMasterCh <- community.SlaveMessage{
+				SenderIP: remoteIP,
+				Payload:  "disconnected",
+			}
+			return
+		case data := <-toSlaveCh:
+			jsonBytes, err := json.Marshal(data)
+			if err != nil {
+				fmt.Println("json.Marshal error: ", err)
+				continue
+			}
+
+			_, err = slaveConn.Write(jsonBytes)
+			if err != nil {
+				fmt.Println("Error:", err)
+				fmt.Println("Closing connection to", slaveConn.RemoteAddr().String())
+				quitCh <- struct{}{}
+				return
+			}
+		case msg := <-tcpReadCh:
+			var data interface{}
+			var elevatorState community.ElevatorState
+			var buttonEvent community.ButtonEvent
+			var orderComplete community.OrderComplete
+
+			if err := json.Unmarshal(msg, &elevatorState); err == nil {
+				data = elevatorState
+			} else if err = json.Unmarshal(msg, &buttonEvent); err == nil {
+				data = buttonEvent
+			} else if err = json.Unmarshal(msg, &orderComplete); err == nil {
+				data = orderComplete
+			} else {
+				fmt.Println("json.Unmarshal error (no matching data types) : ", err)
+				continue
+			}
+
+			if data != nil {
+				toMasterCh <- community.SlaveMessage{
+					SenderIP: remoteIP,
+					Payload:  data,
+				}
+			}
+		}
+	}
+}
+
+func tcpReader(slaveConn *net.TCPConn, ch chan<- []byte, quitCh chan<- struct{}) {
 	buffer := make([]byte, 1024)
 
 	for {
@@ -16,53 +102,13 @@ func Receiver(slaveConn *net.TCPConn, ch chan<- community.SlaveMessage) {
 		n, err := slaveConn.Read(buffer)
 		if err != nil {
 			fmt.Println("Error:", err)
+			fmt.Println("Closing connection to", slaveConn.RemoteAddr().String())
+			quitCh <- struct{}{}
 			return
 		}
-
-		//attempt to unmarshal to different formats:
-		var data interface{}
-		var elevatorState community.ElevatorState
-		var buttonEvent community.ButtonEvent
-		var orderComplete community.OrderComplete
-
-		if err = json.Unmarshal(buffer[:n], &elevatorState); err == nil {
-			data = elevatorState
-		} else if err = json.Unmarshal(buffer[:n], &buttonEvent); err == nil {
-			data = buttonEvent
-		} else if err = json.Unmarshal(buffer[:n], &orderComplete); err == nil {
-			data = orderComplete
-		} else {
-			fmt.Println("json.Unmarshal error (no matching data types) : ", err)
-			return
-		}
-
-		if data != nil {
-			ch <- community.SlaveMessage{
-				SenderIP: remoteIP,
-				Payload:  data,
-			}
-		}
-
+		ch <- buffer[:n]
 	}
 
 }
 
-func Sender(slaveConn *net.TCPConn, ch <-chan json.Marshaler) {
 
-	for {
-		data := <-ch
-		jsonBytes, err := json.Marshal(data)
-		if err != nil {
-			fmt.Println("json.Marshal error: ", err)
-			return
-		}
-
-		_, err = slaveConn.Write(jsonBytes)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-
-	}
-
-}
