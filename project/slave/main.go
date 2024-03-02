53ea0d05d187a2d5a8e53a1e616e7b3da5372e04
Sune Raknes Sogstad <sune.sogstad@shifthyperloop.com>
2024-03-02 11:27:20 +0100

    s

--- project/slave/main.go
+++ project/slave/main.go
@@ -5,9 +5,10 @@ import (
 	"slave/elevio"
 	"slave/fsm"
 	"slave/mastercom"
+
 	// "slave/testclear"
-	"time"
 	"slave/iodevice"
+	"time"
 )
 
 func main() {
@@ -21,26 +22,24 @@ func main() {
 
 	doorTimer := time.NewTimer(-1)
 
-
 	go elevio.PollButtons(drvButtons)
 	go elevio.PollFloorSensor(drvFloors)
 	go elevio.PollObstructionSwitch(drvObstr)
 
-
 	button_press := make(chan elevio.ButtonEvent)
 	clear_request := make(chan elevio.ButtonEvent)
-	master_requests := make(chan [iodevice.N_FLOORS][iodevice.N_BUTTONS] int)
+	master_requests := make(chan [iodevice.N_FLOORS][iodevice.N_BUTTONS]int)
 	requestedState := make(chan bool)
 	log := make(chan bool)
 	sender := make(chan interface{})
 
 	masterChans := mastercom.Master_channels{
-		ButtonPress: button_press,
-		ClearRequest: clear_request,
+		ButtonPress:    button_press,
+		ClearRequest:   clear_request,
 		MasterRequests: master_requests,
 		RequestedState: requestedState,
-		Log: log,
-		Sender: sender,
+		Log:            log,
+		Sender:         sender,
 	}
 
 	// need tcp connection to master
@@ -67,14 +66,13 @@ func main() {
 			fmt.Printf("%+v\n", a)
 			fsm.OnDoorTimeout(doorTimer, masterChans.ClearRequest)
 
-
 		//moved these from mastercom.go as they are involved with current state
-		case a := <- masterChans.MasterRequests:
+		case a := <-masterChans.MasterRequests:
 			fmt.Println(a, "mottat master request melding")
 			fsm.Requests_clearAll()
 			fsm.Requests_setAll(a, doorTimer, masterChans.ClearRequest)
-			
-		case a := <- masterChans.RequestedState:
+
+		case a := <-masterChans.RequestedState:
 			fmt.Println(a, "sender state til master")
 			mastercom.SendState(sender)
 		}
