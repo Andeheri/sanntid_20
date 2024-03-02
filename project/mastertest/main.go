6e1de91fe74229965ed45f47e007839173ed418e
Simen Haug <simehaug@stud.ntnu.no>
2024-03-02 14:58:09 +0100

    moved common types to project root. Trying to implement communication on master side

--- project/mastertest/main.go
+++ project/mastertest/main.go
@@ -1,29 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"project/master/assigner"
-	"project/master/community"
+	"project/master/slavecomm"
 )
 
 func main() {
-	input := community.CommunityState{
-		HallRequests: [][2]bool{{false, false}, {true, false}, {false, false}, {false, true}},
-		States: map[string]community.ElevatorState{
-			"one": {
-				Behavior:    "moving",
-				Floor:       2,
-				Direction:   "up",
-				CabRequests: []bool{false, false, false, true},
-			},
-			"two": {
-				Behavior:    "idle",
-				Floor:       0,
-				Direction:   "stop",
-				CabRequests: []bool{false, false, false, false},
-			},
-		},
-	}
+	go slavecomm.Manager()
 
-	fmt.Print(assigner.Assign(&input))
+	testch := make(chan interface{})
+	go slavecomm.Listener(12221, testch)
+
+	select {}
 }
