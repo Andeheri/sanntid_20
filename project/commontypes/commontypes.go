182978ac47802f7b11bd038e1c00ad71f148bcd7
Simen Haug <simehaug@stud.ntnu.no>
2024-03-03 10:16:38 +0100

    separated function to turn typetaggedJSON to object

--- project/commontypes/commontypes.go
+++ project/commontypes/commontypes.go
@@ -1,5 +1,11 @@
 package commontypes
 
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
 type ElevatorState struct {
 	Behavior    string `json:"behaviour"`
 	Floor       int    `json:"floor"`
@@ -42,4 +48,30 @@ type MOSIChBundle struct {
 type TypeTaggedJSON struct {
 	TypeId string
 	JSON   []byte
+}
+
+func (ttj *TypeTaggedJSON) ToObject(allowedTypes ...reflect.Type) (interface{}, error) {
+
+	var dataType reflect.Type
+
+	for _, allowedType := range allowedTypes {
+		if ttj.TypeId == allowedType.Name() {
+			dataType = allowedType
+			break
+		}
+	}
+
+	if dataType == nil {
+		return nil, fmt.Errorf("TypeTaggedJSON.TypeId %s does not match any specified types", ttj.TypeId)
+	}
+
+	v := reflect.New(dataType)
+	err := json.Unmarshal(ttj.JSON, v.Interface())
+	if err != nil {
+		return nil, err
+	}
+
+	object := reflect.Indirect(v).Interface()
+
+	return object, nil
 }
