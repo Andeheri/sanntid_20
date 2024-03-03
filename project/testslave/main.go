e8a6f46ffdd85b0fbe6e34755c825756df047ae1
Sune Raknes Sogstad <sune.sogstad@shifthyperloop.com>
2024-03-03 13:55:32 +0100

    structs

--- project/testslave/main.go
+++ project/testslave/main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("Hello, World!")
 	initialMasterAddress := "127.0.0.1:11000"
 	masterAddress := make(chan string)
-	slaveQuit := make(chan bool)
+	slaveQuit := make(chan struct{})
 	go slave.Start(initialMasterAddress, masterAddress, slaveQuit)
 
 	select {
