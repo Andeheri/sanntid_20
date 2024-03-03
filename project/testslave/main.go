8e3742ca32b1aa41a0aba5053d7e7e04524d012d
Sune Raknes Sogstad <sune.sogstad@shifthyperloop.com>
2024-03-03 12:49:05 +0100

    kill slave test

--- project/testslave/main.go
+++ project/testslave/main.go
@@ -9,6 +9,11 @@ func main() {
 	fmt.Println("Hello, World!")
 	initialMasterAddress := "127.0.0.1:11000"
 	masterAddress := make(chan string)
+	slaveQuit := make(chan bool)
+	go slave.Start(initialMasterAddress, masterAddress, slaveQuit)
 
-	slave.Start(initialMasterAddress, masterAddress)
+	select {
+		case <-slaveQuit:
+			fmt.Println("Slave quit")
+	}
 }
