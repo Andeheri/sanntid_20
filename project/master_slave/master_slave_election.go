b87e2233f6b574f06acd6e6c7e2fac4f03a37bde
Sondre Holte <shholte@stud.ntnu.no>
2024-03-04 10:04:00 +0100

    Test 3

--- project/master_slave/master_slave_election.go
+++ project/master_slave/master_slave_election.go
@@ -26,13 +26,14 @@ func Election(localIP string, mseCh chan<- MSE_type, updatedIPAddressCh <-chan m
 	for ip_address_map := range updatedIPAddressCh {
 		fmt.Printf("Master-slave election: %v\n", ip_address_map)
 		role := Unknown
+		highest_ip_int = 0
 		if len(ip_address_map) == 0 {  // Elevator is disconnected
 			last_role = Master
 			last_highest_ip = "127.0.0.1" // Loopback address (Always smaller than a regular IP)
 			mseCh <- MSE_type{Role: Master, IP: "127.0.0.1"}
 			continue
 		}
-		// Possibly need to update role or master to connect to
+
 		for ip_address := range ip_address_map {
 			ip_address_int := IPToNum(ip_address)
 			if (ip_address_int > highest_ip_int){
